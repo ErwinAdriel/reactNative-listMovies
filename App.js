cd4de6ff84b475b0c0d06d300f1966183a6d9c2c
ErwinAdriel <adrielquispe87@gmail.com>
2024-08-18 16:12:56 -0300

    conexion a la API IMDB

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
+import { getMovies } from './lib/data';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import twd from 'twrnc';
+
 
 export default function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies().then((data) => {
+      setMovies(data.results);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={twd`bg-slate-800 flex-1 pt-8 items-center`}>
+      <StatusBar style="light" />
+      {movies.map(movie => (
+        <View key={movie.id}>
+          <Text style={twd`text-white`}>{movie.original_title}</Text> 
+        </View>
+      ))}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
