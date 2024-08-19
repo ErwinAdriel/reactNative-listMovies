3816bd51240131bb2cbbc56398ea2895407aa5ee
ErwinAdriel <adrielquispe87@gmail.com>
2024-08-19 01:13:56 -0300

    Diseño a la vista de populares

--- components/Main.jsx
+++ components/Main.jsx
@@ -19,13 +19,20 @@ export function Main() {
   return (
     <View style={twd`pt-[${insets.top}] pb-[${insets.bottom}] w-full`}>
         <ScrollView>
+            <Text style={twd`text-white font-bold mx-4 text-4xl my-4`}>Populares</Text>
             {movies.map((movie) => (
-                <View key={movie.id} style={twd`mb-4 items-center`}>
-                <Image
-                    source={{uri: movie.poster_path}}
-                    style={twd`w-30 h-60`}
-                />
-                <Text style={twd`text-white`}>{movie.original_title}</Text>
+                <View key={movie.id} style={twd`mb-4 mx-4 flex flex-row`}>
+                    <View style={twd`w-2/5 `}>
+                        <Image
+                            source={{uri: movie.poster_path}}
+                            style={twd`h-60 rounded-2xl	`}
+                        />
+                    </View>
+                    <View style={twd`w-3/5 pl-4 top-2`}>
+                        <Text style={twd`text-white font-bold text-xl`}>{movie.original_title}</Text>
+                        <Text style={twd`text-white capitalize`}>{movie.release_date} | {movie.original_language}</Text>
+                        <Text style={twd`text-white absolute bottom-4 left-0 ml-4`}>{Number(movie.vote_average).toPrecision(2)}</Text>
+                    </View>
                 </View>
             ))}
         </ScrollView>
