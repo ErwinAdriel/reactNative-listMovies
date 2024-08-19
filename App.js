0924e629c8fe8e590ccc07abf2ec916bb3bbf2a7
ErwinAdriel <adrielquispe87@gmail.com>
2024-08-18 22:55:01 -0300

    obteniendo imagen y titulo de api

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { getMovies } from './lib/data';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View, FlatList } from 'react-native';
 import twd from 'twrnc';
 
 
@@ -10,17 +10,21 @@ export default function App() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    getMovies().then((data) => {
-      setMovies(data.results);
+    getMovies().then((movies) => {
+      setMovies(movies);
     });
   }, []);
 
   return (
     <View style={twd`bg-slate-800 flex-1 pt-8 items-center`}>
       <StatusBar style="light" />
-      {movies.map(movie => (
-        <View key={movie.id}>
-          <Text style={twd`text-white`}>{movie.original_title}</Text> 
+      {movies.map((movie) => (
+        <View key={movie.id} style={twd`mb-4 items-center`}>
+          <Image
+            source={{uri: movie.poster_path}}
+            style={twd`w-50 h-60`}
+          />
+          <Text style={twd`text-white`}>{movie.original_title}</Text>
         </View>
       ))}
     </View>
