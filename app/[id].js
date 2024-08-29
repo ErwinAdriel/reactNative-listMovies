230d681b40ed86517c173da9eb539cc27f8a163c
ErwinAdriel <adrielquispe87@gmail.com>
2024-08-28 21:57:01 -0300

    devolviendo id de movie a traves de useLocalSearchParams

--- app/[id].js
+++ app/[id].js
@@ -1,11 +1,14 @@
 import { Text, View } from "react-native";
 import twd from "twrnc";
 import { Link } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 
  export default function Detalle(){
+
+    const { id } = useLocalSearchParams(); 
     return(
         <View style={twd`flex-1 justify-center items-center`}>
-            <Text style={twd`text-white`}>Detalle de movie</Text>
+            <Text style={twd`text-white`}>Detalle de movie {id}</Text>
             <Link href="/" style={twd`text-white`}>
                 Volver
             </Link>
